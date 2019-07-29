84da49bd1298ca72a7e96d0a16301778bb2fe53e
Joe Jarvis <joe.jarvis109@gmail.com>
2019-07-28 22:11:24 -0400

    Update version

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
-/* globals RSSParser, moment */
-const cptitle = document.getElementById("cptitle");
-const cpdesc = document.getElementById("cpdesc");
-const time = document.getElementById("time");
-const date = document.getElementById("date");
-const feed = document.getElementById("feed");
+/* globals moment RSSParser */
+const feedEle = document.getElementById("feed");
 const CORS = "https://cors-anywhere.herokuapp.com/";
-
-function updateTime() {
-    time.innerText = moment().format("hh:mm");
-    date.innerText = moment().format("MMMM Do, YYYY");
-}
+const COLORS = [
+    "#F44336",
+    "#E91E63",
+    "#9C27B0",
+    "#673AB7",
+    "#3F51B5",
+    "#2196F3",
+    "#03A9F4",
+    "#00BCD4",
+    "#009688",
+    "#4CAF50",
+    "#8BC34A",
+    "#CDDC39",
+    "#FFEB3B",
+    "#FFC107",
+    "#FF9800",
+    "#FF5722",
+    "#795548",
+    "#9E9E9E",
+    "#607D8B"
+];
 
 function fillFeed(item, index) {
     // only display 20 items
     if (index > 19) {
         return;
     }
-    let ele = document.createElement("span");
-    ele.className = "item";
 
     let name = document.createElement("a");
-    name.className = "title";
+    name.className = "item";
     name.href = item.link;
     name.innerText = item.title;
     if (item.hasOwnProperty("creator")) {
         name.innerText += " • " + item.creator;
     }
 
-    ele.appendChild(name);
-    feed.appendChild(ele);
+    feedEle.appendChild(name);
+}
+
+function changeTime() {
+    document.getElementById("t").innerText = moment().format("hh:mm A")
+    document.getElementById("d").innerText = moment().format("dddd, MMMM Do, YYYY")
 }
 
 async function main() {
-    // get image from bing
-    let info = await (await fetch(
-        CORS + "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
-    )).json();
-    console.log(info.images[0].url)
-    document.body.style.background = "url(https://bing.com" + info.images[0].url + ")";
-    cptitle.innerText = info.images[0].title;
-    cpdesc.innerText = info.images[0].copyright;
+    // set color
+    document.body.style.backgroundColor =
+        COLORS[Math.floor(Math.random() * COLORS.length)];
 
-    // update clock
-    updateTime();
-    setInterval(updateTime, 1000);
+    // change time
+    changeTime();
+    setInterval(changeTime, 1000);
 
     // get rss feed
     let parser = new RSSParser();
@@ -53,7 +62,7 @@ async function main() {
     } else {
         feedURL = localStorage.feed;
     }
-    let feed = await parser.parseURL(CORS +feedURL);
+    let feed = await parser.parseURL(CORS + feedURL);
     feed.items.forEach(fillFeed);
 }
 
@@ -65,5 +74,11 @@ function changeRSSFeed() {
     }
 }
 
-document.getElementById("changeFeed").addEventListener("click", changeRSSFeed);
+function shortcuts(e) {
+    if (e.key === "c") {
+        changeRSSFeed();
+    }
+}
+
 document.addEventListener("DOMContentLoaded", main);
+document.addEventListener("keydown", shortcuts);
